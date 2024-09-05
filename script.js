9ed6f8b76f5db3407ca6d2d056de0f8e167c973f
arjun31 <arjunbanerjee13@gmail.com>
2024-09-05 17:55:52 +0530

    added new things and fixed old things

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.getElementsByClassName("sidebar")[0];
-    const close = document.getElementById("close-btn");
+    const closeSidebar = document.getElementById("close-btn");
     const open = document.getElementById("view-btn");
+    const signupButton = document.getElementById("signup-btn");
+    const signupFormContainer = document.getElementsByClassName("signup-container")[0];
+    const closeformButton = document.getElementById("close-btn-form");
+    const closeLoginFormButton = document.getElementById("close-lgin-form");
+    const switchToLoginForm = document.getElementById("switch-login");
+    const formsignin = document.getElementsByClassName("formsignin")[0];
+    const formsignup = document.getElementsByClassName("formsignup")[0];
+    const switchToSignUpForm = document.getElementById("switch-signup");
 
     open.addEventListener('click', () => {
         sidebar.classList.add("active");
-        close.classList.add("visible");
+        closeSidebar.classList.add("visible");
     })
 
-    close.addEventListener('click', () => {
+    closeSidebar.addEventListener('click', () => {
         sidebar.classList.remove("active");
-        close.classList.remove("visible");
+        closeSidebar.classList.remove("visible");
     });
 
+    
+    signupButton.onclick = function () {
+        signupFormContainer.style.transform = "translateY(0)";
+        sidebar.classList.remove("active");
+        closeSidebar.classList.remove("visible");
+    }
+
+    closeformButton.onclick = function () {
+        signupFormContainer.style.transform = "translateY(-100%)";     
+    }
+
+    closeLoginFormButton.onclick = function() {
+        signupFormContainer.style.transform = "translateY(-100%)";
+    }
+
+    switchToLoginForm.onclick = function() {
+        formsignup.style.display = "none";
+        formsignin.style.display = "flex";     
+    }
+
+    switchToSignUpForm.onclick = function() {
+        formsignin.style.display = "none";     
+        formsignup.style.display = "flex";
+    }
 });
